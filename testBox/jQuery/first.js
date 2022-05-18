0983eb99bf554cbc45d53b5ddc076ff3ea3e054b
medhabisht <amayanegi42@gmail.com>
2022-05-18 10:39:29 +0530

    updated

---
+++ testBox/jQuery/first.js
@@ -0,0 +1,33 @@
+/*
+$(document).ready(function(){
+var k = $("#head") ;
+k.css({border :" 3px black solid" });
+/*
+$("button").click(function(){
+//$("#head").fadeOut("fast")} );
+$("#head").fadeIn("slow");
+$("#p1").fadeIn("fast");
+$("#p2").fadeIn("fast")});
+
+
+
+$("button").click(function(){
+    $("#p1").fadeOut();
+    $("#p2").fadeOut("slow");
+ 
+  });
+
+
+});
+
+*/
+
+$(function() {
+
+    $("#bruh").click(function(){
+     $("#ok").slideDown("slow") ;
+    });
+
+
+     
+}) ;
